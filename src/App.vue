d47217a811e11b01a83d84f2dcd42009b11d5b6b
Aris M <arisssmulianto@gmail.com>
2022-11-20 15:11:06 +0700

    add font Rubik to project

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,9 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Rubik', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -22,9 +21,12 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration:none;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+  text-decoration: none;
 }
+
 </style>
