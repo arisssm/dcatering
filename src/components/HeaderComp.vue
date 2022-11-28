2471b41565d09f84eb2696e33dbb65797ae2a8b6
Aris M <arisssmulianto@gmail.com>
2022-11-28 08:31:08 +0700

    testing header components

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,48 @@
 <template>
   <div id="HeaderComp">
-    <h1>Kenapa harus begini ?</h1>
-    <button class="btn btn-lg btn-warning">TEST</button>
+    <nav class="navbar navbar-expand-sm">
+      <div class="container">
+        <router-link to="/" class="navbar-brand">
+          <img class="logo-brand" src="../assets/logo-brand.png" alt="">
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Beranda</router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Layanan Katering
+              </a>
+              <ul class="dropdown-menu">
+                <li><router-link to="/packetservice" class="dropdown-item">Paket Prasmanan</router-link></li>
+                <li><router-link to="/menucatering" class="dropdown-item">Menu Katering</router-link></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <router-link to="/galery" class="nav-link">Galeri</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/blog" class="nav-link">Blog</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/aboutus" class="nav-link">Tentang Kami</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/contact" class="nav-link">Kontak</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signin" class="btn btn-sm mt-1" style="color:#fff; background-color: #46bb33;"> Masuk <i
+                  class="fa fa-sign-in" aria-hidden="true"></i></router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,16 +1,40 @@
 <template>
     <div id="HomePage">
         <HeaderComp/>
+        <BannerComp/>
+        <BenefitComp/>
+        <ServiceComp/>
+        <MenuComp/>
+        <TestimonialComp/>
+        <BlogComp/>
+        <GetPromoComp/>
+        <FooterComp/>
     </div>
 </template>
 
 <script>
-import HeaderComp from '@/components/HeaderComp.vue';
+import HeaderComp from '@/components/HeaderComp.vue'
+import BannerComp from '@/components/BannerComp.vue'
+import BenefitComp from '@/components/BenefitComp.vue'
+import MenuComp from '@/components/MenuComp.vue'
+import ServiceComp from '@/components/ServiceComp.vue'
+import TestimonialComp from '@/components/TestimonialComp.vue'
+import BlogComp from '@/components/BlogComp.vue'
+import GetPromoComp from '@/components/GetPromoComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
 
 export default{
     name :"HomePage",
     components:{
         HeaderComp, 
+        BannerComp,
+        BenefitComp,
+        MenuComp,
+        ServiceComp,
+        TestimonialComp,
+        BlogComp,
+        GetPromoComp,
+        FooterComp,
     },
 };
 </script>
