70ae4dc0afc7557000905b26d4942c27a1bfaad1
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:47:24 +0700

    Add form order page feature

---
+++ src/views/OrderPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="Orderpage">
+        <HeaderComp/>
+        <LoginsessionComp/>
+        <BreadcrumbComp/>
+        <OrderComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import BreadcrumbComp from '@/components/BreadcrumbComp.vue';
+import LoginsessionComp from '@/components/LoginsessionComp.vue';
+import OrderComp from '@/components/OrderComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
+
+export default{
+    name :"OrderPage",
+    components:{
+        HeaderComp,
+        BreadcrumbComp,
+        LoginsessionComp,
+        OrderComp,
+        FooterComp,
+    },
+};
+</script>
