49de955bef8f58a1a748a8b8b5ed1c4738fb0a00
Aris M <arisssmulianto@gmail.com>
2022-12-01 07:59:55 +0700

    change style text-decoration

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
   font-family: 'Rubik', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 
 nav {
@@ -15,13 +14,10 @@ nav {
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
   text-decoration:none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
   text-decoration: none;
 }
 
