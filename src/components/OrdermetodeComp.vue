eb047e4cf84e76efe801e2c6dbc79afb487e73f9
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:44:27 +0700

    Add order metode to service page feature

---
+++ src/components/OrdermetodeComp.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="OrdermetodeComp">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 col-md">
+                    <div class="text-start">
+                        <h5 class="my-3">Cara Pemesanan Katering</h5>
+                        <hr>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi illum tempore fuga. Iure,
+                            accusantium beatae?
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ipsam animi aliquid.
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus quasi placeat eveniet
+                            similique aspernatur sint.
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            4. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae?
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            5. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, molestias.
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            6. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo atque quisquam sunt.
+                        </h6>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        <h6>
+                            7. Lorem ipsum dolor sit amet consectetur.
+                        </h6>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md col-sm-12">
+                    <div class="text-start">
+                        <h5 class="my-3">Ketentuan</h5>
+                        <hr>
+                    </div>
+                    <div class="mb-3 py-2 px-2 rounded-2" style="background-color:#d6fae8;">
+                        1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, rem.<br>
+                        2. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, aut.<br>
+                        3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quos?<br>
+                        4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, molestiae.<br>
+                        5. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, veritatis?<br>
+                    </div>
+                    <div class="text-center">
+                        <img class="img-service" src="../assets/img-serv.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'OrdermetodeComp',
+};
+</script>
+  
