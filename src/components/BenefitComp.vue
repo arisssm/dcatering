9815655045d29221ca8e5c1ef64330ec0875a777
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:11:15 +0700

    Add component benefit to main page

---
+++ src/components/BenefitComp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="BenefitComp">
+        <div class="container my-5">
+            <h4 class="text-center mb-3">SOLUSI KATERING ANDA DENGAN PENUH CITA RASA</h4>
+            <hr>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-6 text-center">
+                    <img class="benefit-img m-2" src="../assets/kualitas.png" alt="Quality">
+                    <h6 class="mt-3"><i class="fa-solid fa-circle-check" style="color:#56fc4a" aria-hidden="true"></i> Kualitas Terjamin</h6>
+                </div>
+                <div class="col-lg-3 col-md-6 col-6 text-center">
+                    <img class="benefit-img m-2" src="../assets/harga.png" alt="Price">
+                    <h6 class="mt-3"><i class="fa-solid fa-circle-check" style="color:#56fc4a" aria-hidden="true"></i> Harga Bersahabat</h6>
+                </div>
+                <div class="col-lg-3 col-md-6 col-6 text-center">
+                    <img class="benefit-img m-2" src="../assets/team.png" alt="Team">
+                    <h6 class="mt-3"><i class="fa-solid fa-circle-check" style="color:#56fc4a" aria-hidden="true"></i> Tim Proffesional</h6>
+                </div>
+                <div class="col-lg-3 col-md-6 col-6 text-center">
+                    <img class="benefit-img m-2" src="../assets/waktu.png" alt="Price">
+                    <h6 class="mt-3"><i class="fa-solid fa-circle-check" style="color:#56fc4a" aria-hidden="true"></i> Tepat Waktu</h6>
+                </div>
+            </div>
+        </div>
+    </div>  
+</template>
+  
+<script>
+export default {
+    name: 'BenefitComp',
+};
+</script>
+  
