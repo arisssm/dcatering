7822999e55fc23f09e22ab2c2ed4a0f84e16c997
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:25:16 +0700

    Add sign in page

---
+++ src/views/SigninPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="SigninPage">
+        <HeaderComp/>
+        <SigninComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import SigninComp from '@/components/SigninComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+export default{
+    name :"SigninPage",
+    components:{
+        HeaderComp,
+        SigninComp,
+        FooterComp,
+    },
+};
+</script>
