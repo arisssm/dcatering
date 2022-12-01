1404b393e9cb8a8ae03bd04bd6502d9fb25ef07a
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:45:35 +0700

    Add detail service page feature

---
+++ src/views/DetailservicePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="DetailservicePage">
+        <HeaderComp />
+        <BreadcrumbComp />
+        <div class="container">
+            <div class="row">
+                <h4 class="mb-5">Katering Pernikahan</h4>
+                <img src="../assets/img-cover-wedding.png" class="img-cover-wedding" alt="">
+            </div>
+        </div>
+        <DetailserviceComp />
+        <FooterComp />
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import BreadcrumbComp from '@/components/BreadcrumbComp.vue';
+import DetailserviceComp from '@/components/DetailserviceComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
+
+export default {
+    name: "DetailservicePage",
+    components: {
+        HeaderComp,
+        BreadcrumbComp,
+        DetailserviceComp,
+        FooterComp,
+    },
+};
+</script>
