7465784a8f487fdee25ca92c77b37ddc7cf689cb
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:30:18 +0700

    Add component about us

---
+++ src/components/AboutusComp.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="AboutusComp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 text-center">
+                    <img class="logo-aboutus" src="../assets/logo-about.png" alt="Logo About Us">
+                </div>
+                <div class="col-lg-8 text-text-start">
+                    <h4 style="text-align: justify;">Lorem ipsum dolor sit amet consectetur.</h4>
+                    <div style="text-align: justify;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nulla non ea tempora
+                        repudiandae maxime maiores aliquam, nam asperiores. Accusamus, dolor quidem illo sit expedita
+                        voluptates quia impedit consectetur aperiam!</div>
+                    <div style="text-align: justify;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi nulla non ea tempora
+                        repudiandae maxime maiores aliquam, nam asperiores. Accusamus, dolor quidem illo sit expedita
+                        voluptates quia impedit consectetur aperiam!</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card text-end border-0 my-3" style="background-color:#d6fae8;">
+                    <div class="row">
+                        <div class="col-md col-sm-12 d-flex">
+                            <div class="card-body text-end body-owner">
+                                <div class="card-text">
+                                    "Kami berusaha memberikan pelayanan yang baik dan menu yang bersahabat untuk acara anda"
+                                </div>
+                                <h6 class="card-title mt-5 ">Devlan Farhaz</h6>
+                                <p class="card-text">Pemilik dCateringaja</p>
+                            </div>
+                            <img src="../assets/owner.png" class="img-owner" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'AboutusComp',
+};
+</script>
+  
