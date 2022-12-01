91c0f77842c87a323691e81d734390a8ccf77124
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:52:48 +0700

    Add confirmation page

---
+++ src/views/ConfirmPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="ConfirmPage">
+        <HeaderComp/>
+        <LoginsessionComp/>
+        <BreadcrumbComp/>
+        <ConfirmComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import BreadcrumbComp from '@/components/BreadcrumbComp.vue';
+import LoginsessionComp from '@/components/LoginsessionComp.vue';
+import ConfirmComp from '@/components/ConfirmComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
+
+export default{
+    name :"ConfirmPage",
+    components:{
+        HeaderComp,
+        BreadcrumbComp,
+        LoginsessionComp,
+        ConfirmComp,
+        FooterComp,
+    },
+};
+</script>
