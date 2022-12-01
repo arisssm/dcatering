f5f44f07fad77d6d857c3860da14b088a0af07e8
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:24:13 +0700

    Add status user login

---
+++ src/components/LoginsessionComp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="LoginsessionComp">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="text-end">
+                        <div class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Halo, <span style="color:#46bb33">Salim</span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Keluar</a></li>
+                                <li><a class="dropdown-item" href="#">Ganti Password</a></li>
+                            </ul>
+                        </div>
+                        <div>Login: 10 Desember 2022 -- <span style="color:#46bb33">22.00 WIB</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginsessionComp",
+};
+</script>
