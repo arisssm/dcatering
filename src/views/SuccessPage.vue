aca66873cee73ea9dec2df0c008be5774c296de0
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:56:25 +0700

    Add success order page

---
+++ src/views/SuccessPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="SuccessPage">
+        <HeaderComp/>
+        <BreadcrumbComp/>
+        <LoginsessionComp/>
+        <SuccessComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import BreadcrumbComp from '@/components/BreadcrumbComp.vue';
+import LoginsessionComp from '@/components/LoginsessionComp.vue';
+import SuccessComp from '@/components/SuccessComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+export default{
+    name :"SuccessPage",
+    components:{
+        HeaderComp,
+        BreadcrumbComp,
+        LoginsessionComp,
+        SuccessComp,
+        FooterComp,
+    },
+};
+</script>
