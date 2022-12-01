8d8b25b0e16b73a4572f9cfc191482fb24103e6a
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:34:24 +0700

    Add page contact

---
+++ src/views/ContactPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="ContactPage">
+        <HeaderComp/>
+        <ContactComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import ContactComp from '@/components/ContactComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+export default{
+    name :"ContactPage",
+    components:{
+        HeaderComp,
+        ContactComp,
+        FooterComp,
+    },
+};
+</script>
