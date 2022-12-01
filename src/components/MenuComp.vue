9bce0c96ac671503e8324870372f3ff55db509a4
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:14:58 +0700

    Add component menu to main page

---
+++ src/components/MenuComp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="MenuComp">
+        <div class="container my-3">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card mb-card">
+                        <img src="../assets/img-menu-1.png" class="card-img-top menu-img" alt="menu-img-1">
+                        <div class="card-body text-center title text-white">
+                            <h6 class="card-title">Nama Menu 1</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card mb-card">
+                        <img src="../assets/img-menu-2.png" class="card-img-top menu-img" alt="menu-img-2">
+                        <div class="card-body text-center title text-white">
+                            <h6 class="card-title">Nama Menu 2</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                    <div class="card mb-card">
+                        <img src="../assets/img-menu-3.png" class="card-img-top menu-img" alt="menu-img-3">
+                        <div class="card-body text-center title text-white">
+                            <h6 class="card-title">Nama Menu 3</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuComp',
+};
+</script>
