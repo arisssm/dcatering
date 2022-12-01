a5b74c62a068c35d6f7e2a5f5fd97b26abd1f81a
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:38:09 +0700

    Add component article to blog page

---
+++ src/components/ArticleblogComp.vue
@@ -0,0 +1,142 @@
+<template>
+    <div id="ArticleblogComp">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card mb-3 border-0" style="background-color:#d6fae8;">
+                        <div class="row g-0">
+                            <div class="col-md-3 col-sm-12">
+                                <img src="../assets/img-blog-1.png" class="img-blog"
+                                    alt="img-blog">
+                            </div>
+                            <div class="col-md-9 col-sm-12">
+                                <div class="card-body ms-3">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Beatae, unde.</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Consectetur veritatis quae soluta. Accusantium officia necessitatibus fuga
+                                        veniam possimus optio omnis!</p>
+                                    <p class="card-text"><small class="text-muted">10 Desember 2022 - 22.00 WIB</small>
+                                    </p>
+                                    <router-link to="/detailblog" class="text-decoration-none text-danger"> Baca
+                                        Selengkapnya </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 border-0" style="background-color:#d6fae8;">
+                        <div class="row g-0">
+                            <div class="col-md-3 col-sm-12">
+                                <img src="../assets/img-blog-2.png" class="img-blog"
+                                    alt="img-blog">
+                            </div>
+                            <div class="col-md-9 col-sm-12">
+                                <div class="card-body ms-3">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Beatae, unde.</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Consectetur veritatis quae soluta. Accusantium officia necessitatibus fuga
+                                        veniam possimus optio omnis!</p>
+                                    <p class="card-text"><small class="text-muted">10 Desember 2022 - 22.00 WIB</small>
+                                    </p>
+                                    <router-link to="/detailblog" class="text-decoration-none text-danger"> Baca
+                                        Selengkapnya </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 border-0" style="background-color:#d6fae8;">
+                        <div class="row g-0">
+                            <div class="col-md-3 col-sm-12">
+                                <img src="../assets/img-blog-3.png" class="img-blog"
+                                    alt="img-blog">
+                            </div> 
+                            <div class="col-md-9 col-sm-12">
+                                <div class="card-body ms-3">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Beatae, unde.</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Consectetur veritatis quae soluta. Accusantium officia necessitatibus fuga
+                                        veniam possimus optio omnis!</p>
+                                    <p class="card-text"><small class="text-muted">10 Desember 2022 - 22.00 WIB</small>
+                                    </p>
+                                    <router-link to="/detailblog" class="text-decoration-none text-danger"> Baca
+                                        Selengkapnya </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 border-0" style="background-color:#d6fae8;">
+                        <div class="row g-0">
+                            <div class="col-md-3 col-sm-12">
+                                <img src="../assets/img-blog-1.png" class="img-blog"
+                                    alt="img-blog">
+                            </div>
+                            <div class="col-md-9 col-sm-12">
+                                <div class="card-body ms-3">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Beatae, unde.</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Consectetur veritatis quae soluta. Accusantium officia necessitatibus fuga
+                                        veniam possimus optio omnis!</p>
+                                    <p class="card-text"><small class="text-muted">10 Desember 2022 - 22.00 WIB</small>
+                                    </p>
+                                    <router-link to="/detailblog" class="text-decoration-none text-danger"> Baca
+                                        Selengkapnya </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-3 border-0" style="background-color:#d6fae8;">
+                        <div class="row g-0">
+                            <div class="col-md-3 col-sm-12">
+                                <img src="../assets/img-blog-2.png" class="rounded-start img-blog"
+                                    alt="img-blog">
+                            </div>
+                            <div class="col-md-9 col-sm-12">
+                                <div class="card-body ms-3">
+                                    <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Beatae, unde.</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Consectetur veritatis quae soluta. Accusantium officia necessitatibus fuga
+                                        veniam possimus optio omnis!</p>
+                                    <p class="card-text"><small class="text-muted">10 Desember 2022 - 22.00 WIB</small>
+                                    </p>
+                                    <router-link to="/detailblog" class="text-decoration-none text-danger"> Baca
+                                        Selengkapnya </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-center">
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Previous">
+                                            <span aria-hidden="true">&laquo;</span>
+                                        </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#" aria-label="Next">
+                                            <span aria-hidden="true">&raquo;</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ArticleblogComp",
+};
+</script>
