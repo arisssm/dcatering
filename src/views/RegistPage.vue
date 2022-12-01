43a848ec2a8942b70df5b982de6a3a7425e1ec45
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:26:31 +0700

    Add registration page

---
+++ src/views/RegistPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="RegistPage">
+        <HeaderComp/>
+        <RegistrationComp/>
+        <FooterComp/>
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import RegistrationComp from '@/components/RegistrationComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
+
+export default{
+    name :"RegistPage",
+    components:{
+        HeaderComp,
+        RegistrationComp,
+        FooterComp,
+    },
+};
+</script>
