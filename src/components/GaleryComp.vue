03db40e3adcde8d08767f35ab75ec3f151e0bb54
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:33:05 +0700

    Add component galery to page galery

---
+++ src/components/GaleryComp.vue
@@ -0,0 +1,137 @@
+<template>
+    <div id="GaleryComp">
+        <div class="container my-3">
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 1
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-2.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 2
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-3.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 3
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 4
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 1
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-2.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 2
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-3.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 3
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 4
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 1
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-2.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 2
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-3.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 3
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 4
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 1
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-2.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 2
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-3.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 3
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <img src="../assets/img-galeri-1.png" class="img-galeri" alt="">
+                    <div class="text-center title-galeri">
+                        Galeri 4
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GaleryComp",
+};
+</script>
