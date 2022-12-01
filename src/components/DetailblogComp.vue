fc34ac4eae8c0ad8273d22fb6ce47a28e6dff58d
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:39:17 +0700

    add detail article to blog page

---
+++ src/components/DetailblogComp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="DetailblogComp">
+        <div class="container pg-detail-blog">
+            <h4 class="text-start">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+            <img src="../assets/detailblog-img.png" class="detail-blog-img" alt="">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-6 text-start">
+                    <i class="fa fa-user-circle" aria-hidden="true"></i> Author name
+                </div>
+                <div class="col-lg-6 col-md-6 col-6 text-end">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 10 Desember 2022
+                </div>
+            </div>
+            <div class="mt-3" style="text-align:justify">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto molestias voluptatem tenetur, sequi id odio provident hic dicta omnis ex laborum eum non qui, reiciendis asperiores quaerat perspiciatis voluptas deserunt quis tempora obcaecati veniam. Ad, cumque? Aliquid modi maxime voluptate, maiores incidunt officia quasi ad facere laborum molestias possimus vero!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto molestias voluptatem tenetur, sequi id odio provident hic dicta omnis ex laborum eum non qui, reiciendis asperiores quaerat perspiciatis voluptas deserunt quis tempora obcaecati veniam. Ad, cumque? Aliquid modi maxime voluptate, maiores incidunt officia quasi ad facere laborum molestias possimus vero!
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DetailblogComp",
+};
+</script>
