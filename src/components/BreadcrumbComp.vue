5d9aed16ea93d96f495f907b857a5a3f47d19ea6
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:23:04 +0700

    add current page information

---
+++ src/components/BreadcrumbComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="BreadcrumbComp">
+        <div class="container my-5">
+                <ol class="breadcrumb text-start">
+                    <li><router-link to="/" ><i style="color:#46bb33;" class="fa fa-home me-2" aria-hidden="true"></i></router-link></li>
+                    <li class="breadcrumb-item"><router-link to="/" class="text-decoration-none" style="color:#000;">Beranda</router-link></li>
+                    <li class="breadcrumb-item"><router-link to="/" class="text-decoration-none" style="color:#000;">Sub Page</router-link></li>
+                    <li class="breadcrumb-item active" aria-current="page">Halaman saat ini</li>
+                </ol> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"BreadcrumbComp",
+};
+</script>
