82840237e54d0fb407031939e365e1e1bd276440
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:17:11 +0700

    Add component blog to main page

---
+++ src/components/BlogComp.vue
@@ -0,0 +1,68 @@
+<template>
+    <div id="BlogComp">
+        <div class="container mb-5">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="card border-0">
+                        <img src="../assets/img-blog-1.png" class="img-blog" alt="img-blog-1">
+                        <div class="mt-2 hover">
+                            <h6 class="card-title text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                            <p class="card-text text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sed totam sit?</p>
+                            <router-link to="/detailblog" class="btn btn-sm" style="background-color:#d6fae8;">Baca blog
+                                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card border-0">
+                        <img src="../assets/img-blog-1.png" class="img-blog" alt="img-blog-1">
+                        <div class="mt-2 hover">
+                            <h6 class="card-title text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                            <p class="card-text text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sed totam sit?</p>
+                            <router-link to="/detailblog" class="btn btn-sm" style="background-color:#d6fae8;">Baca blog
+                                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card border-0">
+                        <img src="../assets/img-blog-1.png" class="img-blog" alt="img-blog-1">
+                        <div class="mt-2 hover">
+                            <h6 class="card-title text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                            <p class="card-text text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sed totam sit?</p>
+                            <router-link to="/detailblog" class="btn btn-sm" style="background-color:#d6fae8;">Baca blog
+                                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card border-0">
+                        <img src="../assets/img-blog-1.png" class="img-blog" alt="img-blog-1">
+                        <div class="mt-2 hover">
+                            <h6 class="card-title text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h6>
+                            <p class="card-text text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sed totam sit?</p>
+                            <router-link to="/detailblog" class="btn btn-sm" style="background-color:#d6fae8;">Baca blog
+                                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <hr class="mt-2">
+                <div class="row-col text-center mt-3">
+                    <router-link to="/blog" class="btn btn-sm" role="button" style="background-color:#d6fae8;">
+                        <i class="fa-solid fa-eye" aria-hidden="true"></i> Lihat yang lainnya
+                    </router-link>
+                </div>
+            </div>   
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'BlogComp',
+};
+</script>
