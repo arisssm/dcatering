ad233fedf31e88e349755ebff271e9d7d6332c4b
Aris M <arisssmulianto@gmail.com>
2022-12-01 08:09:09 +0700

    Add component banner to main page

---
+++ src/components/BannerComp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="BannerComp">
+        <div class="container-fluid bg-banner mt-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 text-start banner-sp">
+                        <h4 class="">Ciptakan suasana harmonis acara anda dengan pelayanan katering dari kami</h4>
+                        <p class="">Kami berusaha memberikan pelayanan yang terbaik untuk acara anda</p>
+                        <router-link to="/packetservice" class="btn btn-sm btn-banner text-decoration-none">Ayo Pesan</router-link>
+                    </div>
+                    <div class="col-lg-8 text-end">
+                        <img class="cover-img mb-4" src="../assets/cover-img.png" alt="Cover-Banner-dCateringaja">
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'BannerComp',
+};
+</script>
+  
