10a8e2c67605c5dc8fdba4d529c93436b459fa34
Aris M <arisssmulianto@gmail.com>
2022-12-01 11:40:22 +0700

    Add new page detailbirthday

---
+++ src/views/DetailbirthdayPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="DetailbirthdayPage">
+        <HeaderComp />
+        <BreadcrumbComp />
+        <div class="container">
+            <div class="row">
+                <h4 class="mb-5">Katering Ulang Tahun</h4>
+                <img src="../assets/img-cover-birthday.png" class="img-cover-birthday" alt="">
+            </div>
+        </div>  
+        <DetailserviceComp />
+        <FooterComp />
+    </div>
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import BreadcrumbComp from '@/components/BreadcrumbComp.vue';
+import DetailserviceComp from '@/components/DetailserviceComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
+
+export default {
+    name: "DetailbirthdayPage",
+    components: {
+        HeaderComp,
+        BreadcrumbComp,
+        DetailserviceComp,
+        FooterComp,
+    },
+};
+</script>
